33496b73cf36a43c282ea5ba96bf5af9f5e8f708
asuvorova <mezzo666@mail.ru>
2021-12-21 16:58:03 +0300

    add  IconPropSize - 'l';

--- src/icons/Code/Code.tsx
+++ src/icons/Code/Code.tsx
@@ -3,7 +3,7 @@ import { createIcon } from '../_createIcon/createIcon';
 import CodeSizeAll from './Code_size_all';
 
 export const Code = createIcon({
-  l: CodeSizeAll
+  l: CodeSizeAll,
   m: CodeSizeAll,
   s: CodeSizeAll,
   xs: CodeSizeAll,
