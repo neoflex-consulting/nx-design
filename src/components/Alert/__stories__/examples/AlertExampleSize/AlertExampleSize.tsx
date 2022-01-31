7ad55a423ac810ddb94d46e2e0acf86ac7637454
hedaira <mezzo666@mail.ru>
2022-01-31 13:16:57 +0300

    updare Alert

--- src/components/Alert/__stories__/examples/AlertExampleSize/AlertExampleSize.tsx
+++ src/components/Alert/__stories__/examples/AlertExampleSize/AlertExampleSize.tsx
@@ -8,14 +8,21 @@ export const AlertExampleSize = () => (
   <div>
     <StoryBookExample className={cnDocsDecorator('Section')}>
       <Alert
-        status="system"
+        status="success"
+        view="filled"
+        title="Размер l"
+        label="Это сообщение поменьше"
+        size="l"
+      />
+      <Alert
+        status="success"
         view="filled"
         title="Размер m"
         label="Это обычное сообщение, и размер обычный"
         size="m"
       />
       <Alert
-        status="system"
+        status="success"
         view="filled"
         title="Размер s"
         label="Это сообщение поменьше"
