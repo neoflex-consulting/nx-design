40aa6651b942cca7029d11310f9d3e805c11e170
asuvorova <mezzo666@mail.ru>
2022-08-25 14:37:33 +0300

    add Inter  font

--- src/components/Theme/presets/presetDatagram.ts
+++ src/components/Theme/presets/presetDatagram.ts
@@ -2,7 +2,7 @@ import '../Theme.css';
 import '../_color/Theme_color_gpnDatagram.css';
 import '../_color/Theme_color_gpnDark.css';
 import '../_control/Theme_control_gpnDefault.css';
-import '../_font/Theme_font_gpnDefault.css';
+import '../_font/Theme_font_gpnDatagram.css';
 import '../_size/Theme_size_gpnDefault.css';
 import '../_space/Theme_space_gpnDefault.css';
 import '../_shadow/Theme_shadow_gpnDefault.css';
@@ -16,7 +16,7 @@ export const presetDatagram: ThemePreset = {
     invert: 'gpnDatagram',
   },
   control: 'gpnDefault',
-  font: 'gpnDefault',
+  font: 'gpnDatagram',
   size: 'gpnDefault',
   space: 'gpnDefault',
   shadow: 'gpnDefault',
