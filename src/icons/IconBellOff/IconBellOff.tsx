8a9fa6c56472e769a5853e7de75e952897b634b6
asuvorova <mezzo666@mail.ru>
2022-09-08 22:53:43 +0300

    update icons

--- src/icons/IconBellOff/IconBellOff.tsx
+++ src/icons/IconBellOff/IconBellOff.tsx
@@ -1,14 +1,14 @@
 import { createIcon } from '../_createIcon/createIcon';
 
-import IconBellSizeL from './IconBell_size_l';
-import IconBellSizeM from './IconBell_size_m';
-import IconBellSizeS from './IconBell_size_s';
-import IconBellSizeXs from './IconBell_size_xs';
+import IconBellOffSizeL from './IconBellOff_size_l';
+import IconBellOffSizeM from './IconBellOff_size_m';
+import IconBellOffSizeS from './IconBellOff_size_s';
+import IconBellOffSizeXs from './IconBellOff_size_xs';
 
 export const IconBellOff = createIcon({
-  l: IconBellSizeL,
-  m: IconBellSizeM,
-  s: IconBellSizeS,
-  xs: IconBellSizeXs,
+  l: IconBellOffSizeL,
+  m: IconBellOffSizeM,
+  s: IconBellOffSizeS,
+  xs: IconBellOffSizeXs,
   name: 'IconBellOff',
 });
